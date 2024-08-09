e2256f29c4635f8c00209f83438467343f6270ab
shihabshahrier <shahrier13@yahoo.com>
2024-08-09 19:37:24 +0600

    init

---
+++ todo/src/components/form.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+    return (
+        <div>
+            <h1>Todo App</h1>
+            <form>
+                <input type="text" placeholder="Enter a task" />
+                <button>Add</button>
+            </form>
+        </div>
+    );
+}
